ce75d4000d6d229a9afb32deb54562bc59e3bb61
descholar-ceo <emmanuellamugi@gmail.com>
2024-02-12 20:08:43 +0200

    Use array for passing param

--- src/gato/runner.cr
+++ src/gato/runner.cr
@@ -1,18 +1,18 @@
 module Gato
   class Runner
-    def self.start(arg0 : NamedTuple(queue_name: String, block: JSON::Any ->)) : Nil
+    def self.start(param : Array(NamedTuple(queue_name: String, block: JSON::Any ->))) : Nil
       Log.notice { "El gato esta maullando..." }
 
       AMQP::Client.start(Gato.configuration.amqp_url.to_s) do |c|
         c.channel do |ch|
-          q = ch.queue arg0[:queue_name]
+          q = ch.queue param[0][:queue_name]
           ch.prefetch count: 1
 
           q.subscribe(no_ack: false, block: true) do |msg|
             message = JSON.parse msg.body_io.to_s
             Log.notice { "Received a new Message" }
             
-            arg0[:block].call message
+            param[0][:block].call message
             
             Log.notice { "Done" }
           end
