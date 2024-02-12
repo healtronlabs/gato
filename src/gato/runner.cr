39cb22b7970ede8e6a0f3f77bc0409d177d73e04
descholar-ceo <emmanuellamugi@gmail.com>
2024-02-12 22:27:15 +0200

    It fixes #1

--- src/gato/runner.cr
+++ src/gato/runner.cr
@@ -4,15 +4,17 @@ module Gato
       Log.notice { "El gato esta maullando..." }
       AMQP::Client.start(Gato.configuration.amqp_url.to_s) do |c|
         c.channel do |ch|
-          q = ch.queue param[0][:queue_name]
-          ch.prefetch count: 1
-
-          q.subscribe(no_ack: false, block: true) do |msg|
-            message = JSON.parse msg.body_io.to_s
-            Log.notice { "Received a new message for #{param[0][:queue_name]} queue" }
-            param[0][:message_handler].call message
-            msg.ack
-            Log.notice { "Done processing the new message!" }
+          param.each do |curr_param|
+            q = ch.queue curr_param[:queue_name]
+            ch.prefetch count: 1
+  
+            q.subscribe(no_ack: false, block: true) do |msg|
+              message = JSON.parse msg.body_io.to_s
+              Log.notice { "Received a new message for #{curr_param[:queue_name]} queue" }
+              curr_param[:message_handler].call message
+              msg.ack
+              Log.notice { "Done processing the new message!" }
+            end
           end
         end
       end
