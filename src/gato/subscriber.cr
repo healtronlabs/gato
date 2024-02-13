2b3df2b2e4dc670cf811a705b89d2157fa6f4078
descholar-ceo <emmanuellamugi@gmail.com>
2024-02-13 15:35:34 +0200

    Add publisher class, it fixes #3

--- src/gato/subscriber.cr
+++ src/gato/subscriber.cr
@@ -1,19 +1,25 @@
 module Gato
-  class Runner
-    def self.start(param : Array(NamedTuple(queue_name: String, message_handler: JSON::Any ->))) : Nil
-      Log.notice { "El gato esta maullando..." }
+  class Subscriber
+    def self.subscribe(param : Array(NamedTuple(queue_name: String, message_handler: JSON::Any ->))) : Nil
+      unless LuckyEnv.production?
+        Log.notice { "El gato esta maullando..." }
+      end
       AMQP::Client.start(Gato.configuration.amqp_url.to_s) do |c|
         c.channel do |ch|
           param.each do |curr_param|
             q = ch.queue curr_param[:queue_name], durable: true
             ch.prefetch count: 1
-  
+
             q.subscribe(no_ack: false, block: false) do |msg|
               message = JSON.parse msg.body_io.to_s
-              Log.notice { "Received a new message for #{curr_param[:queue_name]} queue" }
+              unless LuckyEnv.production?
+                Log.notice { "Received a new message for #{curr_param[:queue_name]} queue" }
+              end
               curr_param[:message_handler].call message
               msg.ack
-              Log.notice { "Done processing the new message!" }
+              unless LuckyEnv.production?
+                Log.notice { "Done processing the new message!" }
+              end
             end
           end
           sleep
